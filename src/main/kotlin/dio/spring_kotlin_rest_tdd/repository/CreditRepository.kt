f598e30ec2b8033672d9da14fae879c310285bcb
Guilherme de Oliveira - Giggs <smo.guilherme93@gmail.com>
2023-10-23 15:12:51 -0300

    refactor: Credit Repository.

--- src/main/kotlin/dio/spring_kotlin_rest_tdd/repository/CreditRepository.kt
+++ src/main/kotlin/dio/spring_kotlin_rest_tdd/repository/CreditRepository.kt
@@ -1,10 +1,16 @@
 package dio.spring_kotlin_rest_tdd.repository
 
+import dio.spring_kotlin_rest_tdd.dto.response.CreditListDto
+import dio.spring_kotlin_rest_tdd.dto.response.CustomerCreditDto
 import dio.spring_kotlin_rest_tdd.model.Credit
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.Optional
 
 @Repository
 interface CreditRepository: JpaRepository<Credit, Long> {
 
+  fun findAllByCustomerId(customerId: Long): Iterable<CreditListDto>
+
+  fun findOneByCustomerIdAndCreditId(customerId: Long, creditId: Long): Optional<CustomerCreditDto>
 }
