1a7cf584dd123831c6a76a9601525323b2913bf3
Guilherme de Oliveira - Giggs <smo.guilherme93@gmail.com>
2023-10-23 13:20:33 -0300

    refactor: validator function.

--- src/main/kotlin/dio/spring_kotlin_rest_tdd/service/impl/CreditServiceImplementation.kt
+++ src/main/kotlin/dio/spring_kotlin_rest_tdd/service/impl/CreditServiceImplementation.kt
@@ -1,10 +1,12 @@
 package dio.spring_kotlin_rest_tdd.service.impl
 
 import dio.spring_kotlin_rest_tdd.dto.CreditDto
+import dio.spring_kotlin_rest_tdd.exception.BusinessException
 import dio.spring_kotlin_rest_tdd.model.Credit
 import dio.spring_kotlin_rest_tdd.repository.CreditRepository
 import dio.spring_kotlin_rest_tdd.repository.CustomerRepository
 import dio.spring_kotlin_rest_tdd.service.CreditService
+import java.lang.IllegalArgumentException
 import org.springframework.stereotype.Service
 import java.time.LocalDate
 import java.util.*
@@ -17,7 +19,6 @@ class CreditServiceImplementation(
   override fun requestOne(
     params: CreditDto
   ): String {
-//    transformar os parametros todos do requestOne em um DTO e passar esses parametros para validate()
     validate(params)
     val dtoToObject = Credit(
       customerId = params.customerId,
@@ -37,10 +38,11 @@ class CreditServiceImplementation(
     TODO("Not yet implemented")
   }
 
-  private fun validate(data: CreditDto) {
-    if(data.dayOfFirstInstallment > LocalDate.now().plusDays(91)) throw IllegalArgumentException("bottomtext")
-    if(data.creditValue < 0) throw IllegalArgumentException("waytoodank")
-    if(data.numberOfInstallments !in 1..48) throw IllegalArgumentException("feelsbadman")
-    if(customers.findById(data.customerId).isEmpty) throw IllegalArgumentException("whosthere")
+  internal fun validate(data: CreditDto): Boolean {
+    if(data.dayOfFirstInstallment > LocalDate.now().plusDays(91)) throw BusinessException("bottomtext")
+    if(data.creditValue < 0) throw BusinessException("waytoodank")
+    if(data.numberOfInstallments !in 1..48) throw BusinessException("feelsbadman")
+    if(customers.findById(data.customerId).isEmpty) throw BusinessException("whosthere")
+    return true
   }
 }
