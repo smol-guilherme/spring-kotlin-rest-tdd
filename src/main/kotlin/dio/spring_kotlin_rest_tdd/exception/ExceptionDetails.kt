e2c541fb6fdffb357a34d1e13e1f2f7812030708
Guilherme de Oliveira - Giggs <smo.guilherme93@gmail.com>
2023-10-28 14:11:59 -0300

    feat: added exception handlers.

---
+++ src/main/kotlin/dio/spring_kotlin_rest_tdd/exception/ExceptionDetails.kt
@@ -0,0 +1,11 @@
+package dio.spring_kotlin_rest_tdd.exception
+
+import java.time.LocalDateTime
+
+data class ExceptionDetails(
+  val title: String,
+  val timestamp: LocalDateTime,
+  val status: Int,
+  val exception: String,
+  val details: MutableMap<String, String?>
+)
